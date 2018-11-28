e6ebd2d09932aceed93eb5fa834163635428c9e9
jayAardvark <44345078+jayAardvark@users.noreply.github.com>
2018-11-28 12:36:15 -0800

    initial upload of false filter algorithm

---
+++ js/index.js
@@ -0,0 +1,18 @@
+/*
+  removes false boolean from array.  needs modificatoin and then can be useable.
+*/
+
+function bouncer(arr) {
+
+  for (var i = 0; i < arr.length; i++){
+    var check = Boolean(arr[i]);
+    console.log(check);
+    if (check == false){
+      arr.splice(i,1);
+      i -= i;
+    }
+  }console.log(arr);
+  return arr;
+}
+
+bouncer([false, null, 0, NaN, undefined, "",3]);
